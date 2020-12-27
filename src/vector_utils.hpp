554084f3a6cfe14dce1c11e73c108236fc5b9cdd
David Hebbeker <david@hebbeker.info>
2020-12-27 14:56:37 +0100

    Pasted variadic template function to concatenate std::vector containers.
    Copied from https://stackoverflow.com/a/23440311/.

--- src/vector_utils.hpp
+++ src/vector_utils.hpp
@@ -48,4 +48,29 @@ std::vector<T>& operator+=(std::vector<T> &A, const T &B)
   return A;
 }
 
+// Nice syntax to allow in-order expansion of parameter packs.
+struct do_in_order {
+    template<typename T> do_in_order(std::initializer_list<T>&&) { }
+};
+
+namespace details {
+template<typename V> void concat_helper(V& l, const V& r) {
+    l.insert(l.end(), r.begin(), r.end());
+}
+template<class V> void concat_helper(V& l, V&& r) {
+    l.insert(l.end(), std::make_move_iterator(r.begin()),
+             std::make_move_iterator(r.end()));
+}
+} // namespace details
+
+template<typename T, typename... A>
+std::vector<T> concat(std::vector<T> v1, A&&... vr) {
+    std::size_t s = v1.size();
+    do_in_order { s += vr.size() ... };
+    v1.reserve(s);
+    do_in_order { (details::concat_helper(v1, std::forward<A>(vr)), 0)... };
+    return std::move(v1);   // rvo blocked
+}
+
+
 #endif /* VECTOR_UTILS_HPP */
