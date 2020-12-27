ac892fbd0ef3f73c678f0e7d85ec5071e79a6b5e
David Hebbeker <david@hebbeker.info>
2020-12-27 17:54:38 +0100

    Removed unnecessary comparison function.

--- src/dotdirdeps.cpp
+++ src/dotdirdeps.cpp
@@ -141,19 +141,7 @@ struct DotDirProperty
   bool isOriginal = false;
 };
 
-template<typename Function, Function * function>
-struct Functor
-{
-    template<typename Iterator1, typename Iterator2>
-    bool operator()(Iterator1 lhs, Iterator2 rhs) const
-    {
-        return function(lhs, rhs);
-     }
-};
-
-#define SpecializeFunctor(function) Functor<decltype(function), &function>
-
-typedef std::map<const DirDef * const, DotDirProperty, SpecializeFunctor(compareDirDefs)> PropertyMap;
+typedef std::map<const DirDef * const, DotDirProperty> PropertyMap;
 typedef decltype(std::declval<DirDef>().level()) DirectoryLevel;
 
 /**
