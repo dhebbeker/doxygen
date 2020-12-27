6cc85c7977a938f30e98f23bf1d30ca560b8400a
David Hebbeker <david@hebbeker.info>
2020-12-27 16:14:50 +0100

    Move do_in_order into details namespace.

--- src/vector_utils.hpp
+++ src/vector_utils.hpp
@@ -9,12 +9,13 @@
 #include <vector>
 #include <type_traits>
 
+namespace details {
+
 // Nice syntax to allow in-order expansion of parameter packs.
 struct do_in_order {
     template<typename T> do_in_order(std::initializer_list<T>&&) { }
 };
 
-namespace details {
 template<typename V> void concat_helper(V& l, const V& r) {
     l.insert(l.end(), r.begin(), r.end());
 }
@@ -27,9 +28,9 @@ template<class V> void concat_helper(V& l, V&& r) {
 template<typename T, typename... A>
 std::vector<T> concat(std::vector<T> v1, A&&... vr) {
     std::size_t s = v1.size();
-    do_in_order { s += vr.size() ... };
+    details::do_in_order { s += vr.size() ... };
     v1.reserve(s);
-    do_in_order { (details::concat_helper(v1, std::forward<A>(vr)), 0)... };
+    details::do_in_order { (details::concat_helper(v1, std::forward<A>(vr)), 0)... };
     return std::move(v1);   // rvo blocked
 }
 
